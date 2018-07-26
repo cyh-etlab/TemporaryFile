4e1f28ebe2ac1c40ddeddac3f5658964c7dffd53
cyh-etlab <41722972+cyh-etlab@users.noreply.github.com>
2018-07-26 14:45:05 +0800

    main-1.c
    ARM mbed OS 5.9.1 移植在STM32F103C8，簡單按鍵中斷DEMO範例

---
+++ main-1.c
@@ -0,0 +1,19 @@
+#include "mbed.h"
+
+DigitalOut  led1(PA_7);
+DigitalOut  led2(PA_6);
+InterruptIn button(PA_5);
+ 
+void PA5_IRQHandler() 
+{
+    led2 = !led2;
+}
+int main() 
+{
+	button.rise(&PA5_IRQHandler); 
+  while(1)
+	{
+		 led1 = !led1;
+     wait(0.2);
+  }
+}
